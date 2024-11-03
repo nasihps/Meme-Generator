888de9fe15faabb1a57d7d0b745d9cb64fbeda4a
nasihps <nasihps@gmail.com>
2024-11-03 23:37:27 +0530

    added all files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Header from "./components/Header"
+import Footer from"./components/Footer"
+import Meme from "./components/Meme"
+
+export default function App() {
+    return (
+        <div>
+            <Header />
+            <Meme />
+            <Footer />
+        </div>
+    )
+}
